cc89f370522d5e72039e66fcc002e40357f3fe62
Alejandro Luces González <aluces@polytex.local>
2023-08-16 15:17:52 -0400

    Adding_form_validation

--- JavaScript_DOM_Eventos_inicio/JavaScript_DOM_Eventos_inicio/js/script.js
+++ JavaScript_DOM_Eventos_inicio/JavaScript_DOM_Eventos_inicio/js/script.js
@@ -31,6 +31,10 @@ navegacion.appendChild(nuevoEnlace)
 
 // EVENTOS
 
+/* 
+
+
+
 console.log(1);
 
 window.addEventListener('load', function() {//Load espera a que el JS y los archivos que dependen del HTML esten listos
@@ -54,20 +58,129 @@ window.onscroll = function() {//Cada vez que se hace scroll se ejecuta la funcio
     console.log('scrolling...');
 }
 
+
+
+*/
+
+
+
+
 // Seleccionar elementos y asociarles un evento
 
-const btnEnviar = document.querySelector('.boton--primario')
-btnEnviar.addEventListener('click', function(evento) {
-    console.log(evento);
-    evento.preventDefault()//Previene la accion por defecto del elemento
-    console.log('enviando formulario');
+// const btnEnviar = document.querySelector('.boton--primario')
+// btnEnviar.addEventListener('click', function(evento) {
+//     console.log(evento);
+//     evento.preventDefault()//Previene la accion por defecto del elemento
+//     console.log('enviando formulario');
+// }
+// )
+
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
 }
-)
+
+
+const nombre = document.querySelector('#nombre') 
+const email = document.querySelector('#email')
+const mensaje = document.querySelector('#mensaje')
+const formulario = document.querySelector('.formulario')
+
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+// El evento de submit
+formulario.addEventListener('submit', function(evento) {
+    evento.preventDefault()//Previene la accion por defecto del elemento
+    
+    
+
+    // Validar el formulario
+    const {nombre, email, mensaje} = datos
+
+    if(nombre === '' || email === '' || mensaje === '') {
+        
+        mostrarError('Todos los campos son Obligatorios')
+        return; // Corta la ejecucion del codigo
+    }
+
+ // crear la alerta de Enviado Correctamente
+    
+    mostrarMensaje('Enviado Correctamente')
+
+
+    
+
+
+})
 
 
 
+function leerTexto(e){
+    // console.log(e.target.value);
+    datos[e.target.id] = e.target.value;
+
+    // console.log(datos);
+    
+    
+}
+
+// Mostrar alerta de error o de envio correcto en el DOM
+
+function mostrarMensaje(mensaje){
+    const alerta = document.createElement('P')
+    alerta.textContent = mensaje
+    alerta.classList.add('correcto')
+    formulario.appendChild(alerta);
+
+    // Desaparezca despues de 5 segundos
+    setTimeout(() => {
+        alerta.remove()
+    }
+    , 5000);
+
+}
+
+function mostrarError(mensaje) {
+   
+    const error = document.createElement('P')
+    console.log(error);
+    
+    error.textContent = mensaje
+    error.classList.add('error')
+
+    formulario.appendChild(error)
+
+    // Desaparezca despues de 5 segundos
+    setTimeout(() => {
+        error.remove()
+    }, 5000);
 
 
 
+}
 
+// function mostrarAlerta(mensaje, error = null) {
+   
+//     const alerta = document.createElement('P')
+//     console.log(alerta);
+    
+//     alerta.textContent = mensaje
+//     if(error) {
+//         alerta.classList.add('error')
+//     } else {
+//         alerta.classList.add('correcto')
+//     }
+
+//     formulario.appendChild(alerta)
+
+//     // Desaparezca despues de 5 segundos
+//     setTimeout(() => {
+//         alerta.remove()
+//     }, 5000);
+
+
+    
 
